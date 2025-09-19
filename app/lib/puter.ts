57e4894c7eada2c7677137a3287385ca1632ad36
Zakria Khan <zakriakhanz2003@gmail.com>
2025-09-20 01:42:06 +0500

    Update puter.ts

--- app/lib/puter.ts
+++ app/lib/puter.ts
@@ -350,7 +350,7 @@ export const usePuterStore = create<PuterStore>((set, get) => {
           ],
         },
       ],
-      { model: "claude-3-7-opus" }
+      { model: "claude-3-7-sonnet" }
     ) as Promise<AIResponse | undefined>;
   };
 
